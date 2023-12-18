acbd723cbe0bf507b69e8116c4ebc0297a0765ed
KillReall666 <codewarrior666@mail.ru>
2023-12-18 08:59:38 +0300

    Покрытие хендлеров тестами

--- internal/handlers/addurl/addurl.go
+++ internal/handlers/addurl/addurl.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/KillReall666/url-shortener/internal/storage"
 
@@ -26,14 +27,23 @@ func (a *AddURLHandler) AddURL(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 
-	url := string(body)
+	URL := string(body)
+
+	_, err = url.ParseRequestURI(URL)
+	if err != nil {
+		log.Println("invalid URL")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	id, err := shortid.Generate()
 	if err != nil {
 		log.Println(err)
 	}
 
-	a.Storage.URLStore[id] = url
+	a.Storage.URLStore[id] = URL
 
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("http://localhost:8080/" + id))
+
 }
